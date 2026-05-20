169b0a63391da8957f641534e70200c07f52aecc
Mehedi05739 <mehedi05739@gmail.com>
2026-05-20 22:26:28 +0600

    refactor: remove speech-to-text implementation and unused permissions

--- lib/screens/note_screens/add_new_note_page.dart
+++ lib/screens/note_screens/add_new_note_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_note_app/widgets/text_edit_widget.dart';
-// import 'package:speech_to_text/speech_recognition_result.dart';
-// import 'package:speech_to_text/speech_to_text.dart';
 
 import '../../controller/note_controller.dart';
 
@@ -14,29 +12,12 @@ class AddNewNotePage extends StatefulWidget {
 }
 
 class _AddNewNotePageState extends State<AddNewNotePage> {
-  // final SpeechToText _speechToText = SpeechToText();
-  String _lastWord = '';
-
   @override
   void initState() {
     super.initState();
     Get.find<NoteController>().titleController.text = '';
     Get.find<NoteController>().contentController.text = '';
   }
-
-  // Future<void> voiceAction() async {
-  //   bool available = await _speechToText.initialize(
-  //     onStatus: (_) {},
-  //     onError: (_) {},
-  //   );
-  //   if (available) {
-  //     _speechToText.listen(onResult: _resultListener);
-  //   }
-  // }
-  //
-  // void _resultListener(SpeechRecognitionResult result) {
-  //   setState(() => _lastWord = result.recognizedWords);
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -55,20 +36,10 @@ class _AddNewNotePageState extends State<AddNewNotePage> {
               ),
             ),
           ),
-          body: Column(
+          body: const Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              if (_lastWord.isNotEmpty)
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 4, 20, 8),
-                  child: Text(
-                    _lastWord,
-                    style: theme.textTheme.bodySmall?.copyWith(
-                      color: theme.hintColor,
-                    ),
-                  ),
-                ),
-              const Expanded(
+              Expanded(
                 child: TextEditWidget(readOnly: false, content: null, isAddNote: true),
               ),
             ],
